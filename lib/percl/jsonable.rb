c29a538031dc19cd83a3fcefd808cc427eca3203
Francis San Filippo <fsanfilipp@vailsys.com>
2020-01-28 16:57:53 -0600

    fix JSONable to stringify arrays (prompts)

--- lib/percl/jsonable.rb
+++ lib/percl/jsonable.rb
@@ -1,14 +1,23 @@
 class JSONable
   def as_json
-      hash = {}
-      self.instance_variables.each do |var|
-          hash[var.to_s.delete "@"] = self.instance_variable_get var
+    hash = {}
+    self.instance_variables.each do |var|
+      val = self.instance_variable_get var
+      if val.instance_of? Array
+        arr = []
+        val.each do |prompt|
+          arr.push prompt.as_json
+        end
+        hash[var.to_s.delete "@"] = arr
+      else
+        hash[var.to_s.delete "@"] = val
       end
-      {self.class.name.split('::').last => hash}
+    end
+    {self.class.name.split('::').last => hash}
   end
   def from_json! string
-      JSON.load(string).each do |var, val|
-          self.instance_variable_set var, val
-      end
+    JSON.load(string).each do |var, val|
+      self.instance_variable_set var, val
+    end
   end
 end
