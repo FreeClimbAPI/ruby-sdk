d2ab7778e7b3127f8adc96b7ebcc4d25ce20e775
Raymond Chi <rchi@vailsys.com>
2022-07-22 09:48:58 -0500

    Add loop field to quickstart say command

--- spec/quickstart_spec.rb
+++ spec/quickstart_spec.rb
@@ -5,6 +5,6 @@ describe "quickstart" do
         say = Freeclimb::Say.new(text:'Hello, World!')
         script = Freeclimb::PerclScript.new(commands:[say])
         json = script.to_json()
-        expect(json).to eq("[{\"Say\":{\"text\":\"Hello, World!\"}}]")
+        expect(json).to eq("[{\"Say\":{\"text\":\"Hello, World!\",\"loop\":1}}]")
     end
 end
