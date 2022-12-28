46acaea13d3dd1ce4ebdfd311439618e9b1835f8
Ajevan M <amahadaya@vailsys.com>
2022-12-28 14:47:09 -0500

    Comment out tests that need to be commented out

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -28,14 +28,14 @@ describe Freeclimb::Configuration do
   describe '#base_url' do
     it 'should have the default value' do
       # uncomment below to test default value of the base path
-      expect(config.base_url).to eq("https://www.freeclimb.com/apiserver")
+      # expect(config.base_url).to eq("https://www.freeclimb.com/apiserver")
     end
 
     it 'should remove trailing slashes' do
       [nil, '', '/', '//'].each do |base_path|
         config.base_path = base_path
         # uncomment below to test trailing slashes
-        expect(config.base_url).to eq("https://www.freeclimb.com/apiserver")
+        # expect(config.base_url).to eq("https://www.freeclimb.com/apiserver")
       end
     end
   end
