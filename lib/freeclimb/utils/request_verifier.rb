1013398b450dd95555820cdedeb44c5d2d181bb1
Ajevan M <amahadaya@vailsys.com>
2023-04-04 17:02:55 -0400

    Making methods private as per PR comment and spec definition

--- lib/freeclimb/utils/request_verifier.rb
+++ lib/freeclimb/utils/request_verifier.rb
@@ -3,24 +3,27 @@ module Freeclimb
         class << self
             @@DEFAULT_TOLERANCE = 5 * 60 * 1000
             
-            def verify_request_signature(request_body, request_header, signing_secret, tolerance=DEFAULT_TOLERANCE)
-                check_request_body(request_body)
-                check_request_header(request_header)
-                check_signing_secret(signing_secret)
-                check_tolerance(tolerance)
+            public def verify_request_signature(request_body, request_header, signing_secret, tolerance=DEFAULT_TOLERANCE)
+                request_verifier_object = Freeclimb::RequestVerifier.new()
+                request_verifier_object.instance_eval{ check_request_body(request_body) }
+                request_verifier_object.instance_eval{ check_request_header(request_header) }
+                request_verifier_object.instance_eval{ check_signing_secret(signing_secret) }
+                request_verifier_object.instance_eval{ check_tolerance(tolerance) }
                 info = Freeclimb::SignatureInformation.new(request_header)
-                verify_tolerance(info, tolerance)
-                verify_signature(info, request_body, signing_secret)
+                request_verifier_object.instance_eval{ verify_tolerance(info, tolerance) }
+                request_verifier_object.instance_eval{ verify_signature(info, request_body, signing_secret)}
             end
         end
-            
-        def self.check_request_body(request_body)
+        
+        private
+
+        def check_request_body(request_body)
             if request_body == "" || request_body == nil
                 raise 'Request Body cannot be empty or null'
             end
         end
 
-        def self.check_request_header(request_header)
+        def check_request_header(request_header)
             if request_header == "" || request_header == nil
                 raise 'Error with request header, Request header is empty'
             elsif !(request_header.include? "t")
@@ -30,30 +33,29 @@ module Freeclimb
             end
         end
 
-        def self.check_signing_secret(signing_secret)
+        def check_signing_secret(signing_secret)
             if signing_secret == "" || signing_secret == nil
                 raise 'Signing secret cannot be empty or null'
             end
         end
 
-        def self.check_tolerance(tolerance)
+        def check_tolerance(tolerance)
             if tolerance <= 0 || !(tolerance.is_a? Integer)
                 raise 'Tolerance value must be a positive integer'
             end
         end
 
-        def self.verify_tolerance(info, tolerance)
+        def verify_tolerance(info, tolerance)
             currentTime = info.get_current_unix_time()
             if !info.is_request_time_valid(tolerance)
                 raise "Request time exceeded tolerance threshold. Request: " + info.request_timestamp.to_s + ", CurrentTime: " + currentTime.to_s + ", tolerance: " + tolerance.to_s
             end
         end
 
-        def self.verify_signature(info, request_body, signing_secret)
+        def verify_signature(info, request_body, signing_secret)
             if !info.is_signature_safe(request_body, signing_secret)
                 raise "Unverified signature request, If this request was unexpected, it may be from a bad actor. Please proceed with caution. If the request was exepected, please check any typos or issues with the signingSecret"
             end
         end
-        private_class_method :check_request_body, :check_request_header, :check_signing_secret, :check_tolerance, :verify_tolerance, :verify_signature
     end
 end
