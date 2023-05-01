c2e119bb05c2a1667d988d8f978c96d097e45367
amahadaya <118939810+amahadaya@users.noreply.github.com>
2023-05-01 19:53:18 -0400

    Resolve VCSWP-19571 (Fix test bug for ruby sdk) (#38)
    * Fix test bug for ruby sdk
    
    * Extract timestamp into variable for ruby sdk

--- spec/utils/signature_information_spec.rb
+++ spec/utils/signature_information_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
-
+$time_stamp = 1679944186
+$FIXNUM_MAX = (2**(0.size * 8 -2) -1)
 describe 'SignatureInformation' do
     before do
-        @request_header = "t=1679944186,v1=c3957749baf61df4b1506802579cc69a74c77a1ae21447b930e5a704f9ec4120,v1=1ba18712726898fbbe48cd862dd096a709f7ad761a5bab14bda9ac24d963a6a8"
+        @request_header = "t=#{$time_stamp},v1=c3957749baf61df4b1506802579cc69a74c77a1ae21447b930e5a704f9ec4120,v1=1ba18712726898fbbe48cd862dd096a709f7ad761a5bab14bda9ac24d963a6a8"
         @signature_information_object = Freeclimb::SignatureInformation.new(@request_header)
     end
     describe '#is_request_time_valid' do
@@ -14,7 +15,7 @@ describe 'SignatureInformation' do
         end
         context 'request time is not within tolerance threshold' do
             it 'returns false since it does not match condition of request time being within tolerance threshold' do
-                tolerance = 5 * 60 * 10000
+                tolerance = $FIXNUM_MAX - $time_stamp
                 expect(@signature_information_object.is_request_time_valid(tolerance)).to be false
             end
         end
